07a3ea4cd7b2c40f647d96739823b11c1a6ba1a4
Adriano Tapia <69440069+Tapiita0401@users.noreply.github.com>
2025-02-10 06:18:17 +0000

    proyecto_final

---
+++ proyecto/controller/report.py
@@ -0,0 +1,20 @@
+from config.app import *
+import pandas as pd
+
+def GenerateReportVentas(app: App):
+    conn = app.bd.getConection()
+    ventas = pd.read_sql_query("SELECT * FROM VENTAS", conn)
+    postalcodes = pd.read_sql_query("SELECT * FROM POSTALCODE", conn)
+
+    reporte = ventas.merge(postalcodes, left_on='postal_code', right_on='code')
+    reporte = reporte.groupby(['pais', 'product_id'])['quantity'].sum().reset_index()
+    reporte = reporte.sort_values('quantity', ascending=False)
+    reporte.columns = ['pais', 'producto', 'total_vendido']
+
+    fecha = "10-02-2025" 
+    path = f"/workspaces/practica_python_datux/proyecto/files/reporte_ventas_{fecha}.csv"
+    reporte.to_csv(path, index=False)
+    sendMail(app, path)
+
+def sendMail(app: App, data):
+    app.mail.send_email('from@example.com', 'Reporte de Ventas', 'Reporte de Ventas', data)
