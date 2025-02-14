89a0a3daa4b0d11a63c177dbb34de3b96b65b145
Adriano Tapia <69440069+Tapiita0401@users.noreply.github.com>
2025-02-14 04:15:09 +0000

    codeando_el projectfinal

--- proyecto/controller/report.py
+++ proyecto/controller/report.py
@@ -1,20 +1,19 @@
 from config.app import *
 import pandas as pd
 
-def GenerateReportVentas(app: App):
+def GenerateReportVentasMenosCompras(app: App):
     conn = app.bd.getConection()
     ventas = pd.read_sql_query("SELECT * FROM VENTAS", conn)
     postalcodes = pd.read_sql_query("SELECT * FROM POSTALCODE", conn)
 
     reporte = ventas.merge(postalcodes, left_on='postal_code', right_on='code')
-    reporte = reporte.groupby(['pais', 'product_id'])['quantity'].sum().reset_index()
-    reporte = reporte.sort_values('quantity', ascending=False)
-    reporte.columns = ['pais', 'producto', 'total_vendido']
-    
-    fecha = "10-02-2025" 
-    path = f"/workspaces/practica_python_datux/proyecto/files/reporte_ventas_{fecha}.csv"
+    reporte = reporte.groupby('pais')['quantity'].sum().reset_index()
+    reporte = reporte.sort_values('quantity', ascending=True)
+    reporte.columns = ['pais', 'total_compras']
+    fecha = "13-02-2025"
+    path = f"/workspaces/practica_python_datux/proyecto/files/reporte_menos_compras_{fecha}.csv"
     reporte.to_csv(path, index=False)
     sendMail(app, path)
-
+    
 def sendMail(app: App, data):
-    app.mail.send_email('from@example.com', 'Reporte de ventas', 'Reporte de ventas', data)
+    app.mail.send_email('from@example.com', 'Reporte de compras mínimas', 'Listado de países con menor cantidad de compras', data)
